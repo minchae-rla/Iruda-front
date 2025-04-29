ce15b78e2c239a7c9b65958049fb414819fba7dc
kimminchae <0720kmc@naver.com>
2025-04-29 23:52:37 +0900

    20250429 프론트 메인화면 작업중

--- src/pages/main/Home.tsx
+++ src/pages/main/Home.tsx
@@ -6,11 +6,25 @@ export const Home = () => {
   const handleLogin = ()=> {
     navigate("/login");
   }
+  const handleSingUp = ()=> {
+    navigate("/signUp")
+  }
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-100">
-      afdsff
-      <img src='/img/logo'></img>
-      <button onClick={handleLogin}>login</button>
+    <div className="flex justify-center items-center flex-col h-screen bg-white space-y-4">
+      <div><img src='src\img\logo.jpg' className='w-32 h-auto'></img></div>
+      <div className='w-[220px] h-auto space-y-2'>
+        <p className='text-2xl text-black text-left'>계획했던 일들을</p>
+        <h1 className='text-5xl text-black text-right font-medium'>"이루다"</h1>
+      </div>
+      <div className='flex justify-center items-center space-x-4'>
+      <button onClick={handleLogin} className='rounded bg-blue-800 w-[110px] h-[40px] text-2xl font-medium text-white hover:bg-blue-900'>로그인</button>
+      <button onClick={handleSingUp} className='rounded bg-blue-800 w-[110px] h-[40px] text-2xl font-medium text-white hover:bg-blue-900'>회원가입</button>
+      </div>
+      <div className='flex space-x-7'>
+        <img src='src\img\kakao.png' className='w-16 h-auto'></img>
+        <img src='src\img\google.png' className='w-16 h-auto'></img>
+      </div>
+      
     </div>
   );
 };
--- src/pages/main/Login.tsx
+++ src/pages/main/Login.tsx
@@ -8,29 +8,9 @@ const Login = () => {
   }
   return (
     <div className="flex justify-center items-center h-screen bg-gray-100">
-      <main className="w-[1080px] h-[700px] flex flex-col justify-center items-center bg-white">
-        {/* Logo */}
-        <div className="logo">
-          <img src="../../img/logo.jpg" alt="logo" className="w-[85px] h-[85px]" />
-        </div>
-
-        {/* Ment */}
-        <div className="ment text-center mb-8">
-          <span className="text-2xl block">계획했던 일들을</span>
-          <span className="text-5xl font-medium block mt-4 ml-[60px]">"이루다"</span>
-        </div>
-
-        {/* Login Buttons */}
-        <div className="login mb-8">
-          <button className="bg-blue-700 text-white py-2 px-4 rounded-lg text-lg mr-4 mb-4">
-            로그인
-          </button>
           <button onClick={handleSignUp} className="bg-blue-700 text-white py-2 px-4 rounded-lg text-lg mb-4">
             회원가입
           </button>
-          
-        </div>
-      </main>
     </div>
   );
 };
